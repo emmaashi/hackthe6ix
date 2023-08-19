01e2abde181a87987820eb6ad79c9c7b663fcb14
HarguntasBenipal <harguntasbenipal16@gmail.com>
2023-08-19 16:44:11 -0400

    Harguntas

--- Backend/app.js
+++ Backend/app.js
@@ -1,63 +1,13 @@
 //module imports
-const express = require("express");
+
+import express from 'express';
+
+//const express = require("express");
 const app = express();
 const bp = require("body-parser");
 const request = require("request");
-``;
-const apiKey = "9fbfb4c21f13700376f6050045993ca4";
 
-app.post("/scan", (req, res) => {
-  const city = req.body.city;
-  console.log(city);
-  if (city.length === 0) res.send("No City inputted");
-});
+//sets up port 
 
-app.post("/scan", (req, res) => {
-  const province = req.body.province;
-  console.log(province);
-  if (province.length === 0) res.send("No province inputted");
-});
-
-app.post("/scan", (req, res) => {
-  const country = req.body.country;
-  console.log(country);
-  if (country.length === 0) res.send("No Country inputted");
-});
-
-const limit = 5;
-
-const coordinateUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${province},${country}&limit=${limit}&appid=${apiKey}`;
-
-const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
-fetch(coordinateUrl)
-  .then((response) => response.json())
-  .then((locationData) => {
-    const lon = locationData.lon;
-    const lat = locationData.lat;
-  });
-
-fetch(weatherUrl)
-  .then((response) => response.json())
-  .then((weatherData) => {
-    //const locationElement = document.getElementById('location');
-    //const temperatureElement = document.getElementById('temperature');
-    //const descriptionElement = document.getElementById('description');
-    //const humidityElement = document.getElementById('humidity');
-    const location = weatherData.name;
-    const temperature = weatherData.main.temp;
-    const descritpion = weatherData.weather[0].description;
-    const humidity = weatherData.main.humidity;
-
-    //locationElement.textContent = data.name;
-    //temperatureElement.textContent = data.main.temp;
-    //descriptionElement.textContent = data.weather[0].description;
-    //humidityElement.textContent = data.main.humidity;
-  })
-  .catch((error) => {
-    console.log("An error occurred:", error);
-  });
-
-app.get("/scan", (req, res) => {
-  res.send(location, temperature, description, humidity);
-});
+const port = 5000;
+app.listen(port, () => console.log("Server at 5000"));
---
+++ script.js
@@ -0,0 +1,66 @@
+
+function checkScroll() {
+    var startY = $(".navbar").height() * 2; //The point where the navbar changes in px
+  
+    if ($(window).scrollTop() > startY) {
+      $(".navbar").addClass("scrolled");
+    } else {
+      $(".navbar").removeClass("scrolled");
+    }
+  }
+  
+  if ($(".navbar").length > 0) {
+    $(window).on("scroll load resize", function () {
+      checkScroll();
+    });
+  }
+  
+async function getCoordinates(coordinateUrl) {
+    const locationResponse = await fetch(coordinateUrl);
+    var locationData = await locationResponse.json();
+
+    lat = locationData[0].lat;
+    lon = locationData[0].lon;
+}
+
+async function getWeather(weatherUrl){
+    const weatherResponse = await fetch(weatherUrl);
+    var weatherData = await weatherResponse.json();
+    console.log(weatherData);
+
+    weather = weatherData.weather[0].id;
+    date = weatherData.dt;
+    temp = weatherData.main.temp;
+    wind = weatherData.wind[]
+    //rain =
+    //humidity = 
+
+    console.log(temp);
+    
+}
+
+function onSubmit() {
+    var city = document.getElementById("city").value;
+    var province = document.getElementById("province").value;
+    var country = document.getElementById("country").value;
+
+    const limit=5;
+    let lat=0;
+    let lon=0;
+    let weather=0 //weather
+    let date=0 //date (exact)
+    let temp =0 //temperature
+    let wind = 0 //windspeed
+    let rain =0 //rain
+    let humidity = 0; //humidity
+
+    const apiKey="aa411e4a1fd7d03c40d25e75fc3d7e06"
+
+    const coordinateUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city},${province},${country}&limit=${limit}&appid=${apiKey}`;
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    
+    getCoordinates(coordinateUrl);
+    getWeather(weatherUrl);
+}
+
+const port = 5000
