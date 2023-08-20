de4f418c41ae073bb882a7295d376eba04a7249e
emmaashi <112121306+emmaashi@users.noreply.github.com>
2023-08-20 00:27:27 -0400

    Co-authored-by: Kelvin <kkelvinnguyen@users.noreply.github.com> Co-authored-by: thetruederp1209@gmail.com <thetruederp1209@gmail.com> Co-authored-by: HarguntasBenipal <HarguntasBenipal@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// let div = document.createElement('div');
-div.classList.add("donation-wrapper");
-
-var div = document.getElementById("main");
-
 // Function to show the donation wrapper and hide the form-box
 function hide() {
   console.log("hi");
@@ -13,9 +8,7 @@ function hide() {
   donationWrapper.style.display = "block";
 }
 
-// Add event listener to the "Create Account" button
-const createAccountButton = document.querySelector(".btn.create-account");
-createAccountButton.addEventListener("click", showDonationWrapper);
+//import getWeatherFromId from "./prediction.js";
 
 function checkScroll() {
   var startY = $(".navbar").height() * 2; //The point where the navbar changes in px
@@ -33,22 +26,150 @@ if ($(".navbar").length > 0) {
   });
 }
 
-function onSubmit() {
-  let city = document.getElementById("city");
-  let province = document.getElementById("province");
-  let country = document.getElementById("country");
-  let amount = document.getElementById("donation");
+async function getCoordinates(coordinateUrl) {
+  const locationResponse = await fetch(coordinateUrl);
+  var locationData = await locationResponse.json();
 
-  console.log(city);
-  console.log(province);
-  console.log(country);
-  console.log(amount);
+  lat = locationData[0].lat;
+  lon = locationData[0].lon;
+
+  const locData = [lat, lon];
+  return locData;
 }
 
-// Get a reference to the "Confirm" button
-const confirmButton = document.getElementById("confirmButton");
+async function getWeather(weatherUrl) {
+  const weatherResponse = await fetch(weatherUrl);
+  var weatherData = await weatherResponse.json();
 
-// Add an event listener to handle navigation when the button is clicked
-confirmButton.addEventListener("click", function () {
-  window.location.href = "submission.html";
-});
+  var weatherid = weatherData.weather[0].id;
+  var date = weatherData.dt;
+  var tempKelvin = weatherData.main.temp;
+  var humidity = weatherData.main.humidity;
+  var wind = weatherData.wind.speed;
+
+  tempCelsius = convertToMetric(tempKelvin);
+
+  const weathData = [tempCelsius, wind, humidity, weatherid];
+  return weathData;
+}
+
+async function onSubmit() {
+  var city = document.getElementById("city").value;
+  var province = document.getElementById("province").value;
+  var country = document.getElementById("country").value;
+
+  const limit = 5;
+  // var date=0 //date (exact)
+  //var tempKelvin =0 //temperature
+  //var wind = 0 //windspeed
+  //var rain =0 //rain
+  //var humidity = 0; //humidity
+  //var tempCelsius = 0;
+  //var fwi=0;
+
+  const apiKey = "aa411e4a1fd7d03c40d25e75fc3d7e06";
+
+  const coordinateUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city},${province},${country}&limit=${limit}&appid=${apiKey}`;
+
+  var locationDatas = await getCoordinates(coordinateUrl);
+
+  var lat = locationDatas[0];
+  var lon = locationDatas[1];
+
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+  var weatherDatas = await getWeather(weatherUrl);
+
+  var tempCelsius = weatherDatas[0];
+  var wind = weatherDatas[1];
+  var humidity = weatherDatas[2];
+  var weatherid = weatherDatas[3];
+
+  // Calculate FFMC, DMC, DC, ISI, BUI
+  const ffmc = calculateFFMC(weatherid, wind);
+  const dmc = calculateDMC(humidity);
+  const dc = calculateDC(tempCelsius);
+  const isi = calculateISI(wind, weatherid);
+  const bui = calculateBUI(humidity, tempCelsius);
+
+  // Calculate FWI
+  var fwi = calculateFWI(isi, bui);
+  console.log(fwi);
+
+  getWeatherFromId(weatherid);
+  // return weatherid = weatherDatas[3];
+}
+console.log(getWeatherFromId(weatherid));
+
+function convertToMetric(tempKelvin) {
+  return tempKelvin - 273.15;
+}
+
+function calculateFFMC(weatherid, wind) {
+  return 91.9 * Math.exp(0.1386 * weatherid) * (1 - Math.exp(-0.000265 * wind));
+}
+
+function calculateDMC(humidity) {
+  return 7.2 * Math.exp(0.0365 * humidity) - 1;
+}
+
+function calculateDC(tempCelsius) {
+  return 7.9 * Math.exp(0.00412 * tempCelsius) - 1;
+}
+
+function calculateISI(wind, weatherid) {
+  return 0.208 * wind * (100 - weatherid);
+}
+
+function calculateBUI(humidity, tempCelsius) {
+  return 0.1 * humidity * (100 - tempCelsius);
+}
+
+function calculateFWI(isi, bui) {
+  return (isi + bui) / 2;
+}
+
+function getWeatherFromId(weatherid) {
+  const numberlist = weatherid.toString().split("");
+  console.log(numberlist);
+  if (numberlist[0] === "2") {
+    var weather = "Thunderstorm";
+  } else if (numberlist[0] === "3") {
+    var weather = "rain";
+  } else if (numberlist[0] === "5") {
+    var weather = "rain";
+  } else if (numberlist[0] === "6") {
+    var weather = "Snow";
+  } else if (numberlist[0] === "7") {
+    if (weatherid === "701") {
+      var weather = "misty";
+    } else if (weatherid === "711") {
+      var weather = "smokey";
+    } else if (weatherid === "721") {
+      var weather = "hazey";
+    } else if (weatherid === "731") {
+      var weather = "Dusty";
+    } else if (weatherid === "741") {
+      var weather = "foggy";
+    } else if (weatherid === "751") {
+      var weather = "sandy";
+    } else if (weatherid === "761") {
+      var weather = "dusty";
+    } else if (weatherid === "762") {
+      var weather = "ashy";
+    } else if (weatherid === "771") {
+      var weather = "squalls";
+    } else if (weatherid === "781") {
+      var weather = "tornado";
+    }
+  } else if (numberlist[0] === "8") {
+    if (weatherid === "800") {
+      var weather = "clear";
+    } else {
+      var weather = "cloudy";
+    }
+  }
+  console.log(weather);
+}
+
+//getWeatherFromId(weatherid);
